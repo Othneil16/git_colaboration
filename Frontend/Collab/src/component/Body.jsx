9eacaea381e58ee0692ffac21b761822adcaf1f8
Rozarioehiz <rozarioehiz@gmail.com>
2024-01-23 09:57:58 -0800

    welcome page

---
+++ Frontend/Collab/src/component/Body.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Body.css'
+
+const Body = () => {
+  return (
+   <>
+   <div className='main'>
+    <div className="body">
+        <h1>welcome</h1>
+        <button>Get started</button>
+    </div>
+   </div>
+   
+   </>
+  )
+}
+
+export default Body
